85cb2cc40d99fc797eae62a0d73b32813b8936ec
justinsadakhom <32400122+justinsadakhom@users.noreply.github.com>
2019-08-27 00:09:06 -0400

    Add files via upload

---
+++ src/neat/FitnessGenome.java
@@ -0,0 +1,19 @@
+package neat;
+
+/**
+ * Ties a fitness to a Genome. 
+ * This is useful in implementation of Evaluators.
+ * 
+ * @author hydrozoa
+ */
+public class FitnessGenome {
+	
+    public float fitness;
+    public Genome genome;
+
+    public FitnessGenome(Genome genome, float fitness) {
+        
+        this.genome = genome;
+        this.fitness = fitness;
+    }
+}
---
+++ src/neat/GenesisGenomeProvider.java
@@ -0,0 +1,13 @@
+package neat;
+
+/**
+ * Passed along to the Evaluator when beginning a new search. This facility will generate all genomes
+ * in the starting population of the search.
+ * 
+ * @author hydrozoa
+ */
+public interface GenesisGenomeProvider {
+	
+	public Genome generateGenesisGenome();
+
+}
